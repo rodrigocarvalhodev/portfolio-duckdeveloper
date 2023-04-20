405c3350d7d5105ea2f8808698cac27aa44b45ae
Rodrigo Carvalho <rodrigo.carvalho@codescript.com.br>
2023-04-20 11:05:16 -0300

    Adjust mobile content

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -9,12 +9,18 @@ import { ActivatedRoute, Event, NavigationEnd, Router } from '@angular/router';
 export class HeaderComponent {
 
   routerLink!: string;
+  burgerOpened: boolean = false;
 
   constructor(router: Router) {
     router.events.subscribe((event: Event) => {
       if (event instanceof NavigationEnd) {
         this.routerLink = event.urlAfterRedirects;
       }
+      this.burgerOpened = false;
     });
+  }
+
+  openMenuBurger() {
+    this.burgerOpened = !this.burgerOpened;
   }
 }
